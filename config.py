c32626ef512b4679695095c23e83029b46a80dce
shubham0257 <shubhambajaj32@gmail.com>
2019-10-17 21:18:02 +0530

    This used for building connection or API

---
+++ config.py
@@ -0,0 +1,20 @@
+import tweepy
+import logging
+
+consumer_key = ""
+consumer_secret = ""
+access_token = ""
+access_token_secret = ""
+
+def create_api():
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    api = tweepy.API(auth, wait_on_rate_limit=True,
+        wait_on_rate_limit_notify=True)
+    try:
+        api.verify_credentials()
+    except Exception as e:
+        logger.error("Error creating API", exc_info=True)
+        raise e
+    logger.info("API created")
+    return api
